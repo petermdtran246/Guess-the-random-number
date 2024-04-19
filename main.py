f9f45194614f475c47e5df11e05c25b58e4edf5c
petermdtran246 <petermdtran246@gmail.com>
2024-04-19 18:50:11 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# Import the random module
+import random
+def number_guess(num):
+
+
+    # Get a random number between 1-100
+    random_number = random.randint(1, 100)
+
+    # Compare parameter num to the random number
+    if num < random_number:
+        print(f'{num} is too low. Random number was {random_number}')
+    elif num > random_number:
+        print(f'{num} is too high. Random number was {random_number}')
+    else:
+        print(f'{num} is correct!')
+
+
+if __name__ == "__main__":
+    # Use the seed 900 to get the same pseudo random numbers every time
+    random.seed(900)
+
+    user_input = input()
+    tokens = user_input.split()
+    for token in tokens:
+        # Convert the string tokens into integers
+        num = int(token)
+        number_guess(num)
